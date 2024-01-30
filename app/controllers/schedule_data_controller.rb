6e7e5eced28df4b4568bf203fd5cd3606ef2ddd8
potepoteAK <tmp1pote@gmail.com>
2024-01-30 22:48:07 +0900

    予定の新規追加画面のviewとnewアクション、createアクション実装。フラッシュメッセージの表示処理。

--- app/controllers/schedule_data_controller.rb
+++ app/controllers/schedule_data_controller.rb
@@ -4,9 +4,17 @@ class ScheduleDataController < ApplicationController
   end
 
   def new
+    @schedule_datum = ScheduleDatum.new
   end
 
   def create
+    @schedule_datum = ScheduleDatum.new(params.require(:schedule_datum).permit(:title, :start_date, :end_date, :all_day_flg, :memo))
+    if @schedule_datum.save
+      flash[:notice] = "スケジュールを登録しました"
+      redirect_to :schedule_data
+    else
+      render "new"
+    end
   end
 
   def show
