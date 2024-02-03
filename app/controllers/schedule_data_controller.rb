c5bb66b63fb9554a0e3bbc9c19b83f044af0f368
potepoteAK <tmp1pote@gmail.com>
2024-02-03 22:41:16 +0900

    バリデーションの実装。エラーメッセージの日本語化。時間を日本標準時に設定。終日の表示の仕方変更のためmodelにメソッド作成。詳細ページ、編集ページ作成。関連アクション記述。deleteアクションのみ未完成。

--- app/controllers/schedule_data_controller.rb
+++ app/controllers/schedule_data_controller.rb
@@ -18,14 +18,27 @@ class ScheduleDataController < ApplicationController
   end
 
   def show
+    @schedule_datum = ScheduleDatum.find(params[:id])
   end
 
   def edit
+    @schedule_datum = ScheduleDatum.find(params[:id])
   end
 
   def update
+    @schedule_datum = ScheduleDatum.find(params[:id])
+    if @schedule_datum.update(params.require(:schedule_datum).permit(:title, :start_date, :end_date, :all_day_flg, :memo))
+      flash[:notice] = "IDが「#{@schedule_datum.id}」の情報を更新しました"
+      redirect_to :schedule_data
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @schedule_datum = ScheduleDatum.find(params[:id])
+    @schedule_datum.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :schedule_data
   end
 end
--- app/models/schedule_datum.rb
+++ app/models/schedule_datum.rb
@@ -1,2 +1,16 @@
 class ScheduleDatum < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 20}
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  include ActiveModel::Validations
+  validates_with EndDateOrderValidator
+  validates :memo, length: {maximum: 500}
+
+  def all_day_flg_converter(schedule_datum)
+    if schedule_datum.all_day_flg
+      return "○"
+    else
+      return ""
+    end
+  end
 end
